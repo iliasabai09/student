45e4d80d28ecd397111b91782b014e2558aaaee6
linakomf <angelina.gelbling@narxoz.kz>
2024-07-28 21:38:47 +0500

    Angel

--- server/schemas/Order.schema.js
+++ server/schemas/Order.schema.js
@@ -8,9 +8,3 @@ const Order = mongoose.model('Order', new mongoose.Schema({
     cardNumber: {type: String, required: true},
     payTime: {type: String, required: true}
 }));
-
-<<<<<<< HEAD
-module.exports = Order;
-=======
-module.exports = Order;
->>>>>>> 2d7bf04af77552df4d770bb40e2f508685eee9b5
